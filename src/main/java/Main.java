dfd99ef2648e427757a5137654edd1e6beaf3f47
AIRAT1 <ayrat1@mail.ru>
2019-10-02 22:25:31 +0300

    add Person id field

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
         QueueService queue = new QueueService();
         for (int i = 0; i < 10; i++) {
-            queue.push(new Person("Name " + i));
+            queue.push(new Person(String.valueOf(i), i));
         }
 
         while (queue.size() > 0) {
             Person person = queue.pull();
-            System.out.println(person.getName());
+            System.out.println("Name " + person.getName() + "; id " + person.getId());
             System.out.println("Queue size: " + queue.size());
         }
     }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,8 +1,10 @@
 public class Person {
     private String name;
+    private int id;
 
-    public Person(String name) {
+    public Person(String name, int id) {
         this.name = name;
+        this.id = id;
     }
 
     public String getName() {
@@ -11,5 +13,13 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
